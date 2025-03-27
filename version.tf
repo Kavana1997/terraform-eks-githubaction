cdee7d363114897929915c8118975094d6fbe5d9
Kavana1997 <102670832+Kavana1997@users.noreply.github.com>
2025-03-27 23:55:27 +0530

    Update version.tf

--- version.tf
+++ version.tf
@@ -9,4 +9,12 @@ terraform {
       version = "~> 2.23"
     }
   }
+  backend "s3" {
+    bucket         = "my-terraform-state-bucket-unique-name-123"
+    key            = "eks/terraform.tfstate"
+    region         = "ap-south-1"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
+  }
+}
 }
