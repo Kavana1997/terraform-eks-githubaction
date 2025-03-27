cbbc99a9620c8c4ab91b7a81df845f7bf4bb7111
kavananu1996 <1997kavananu@gmail.com>
2025-03-27 11:26:18 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 1.29"
+    }
+  }
+  
+provider "aws" {
+  region = var.region
+}
