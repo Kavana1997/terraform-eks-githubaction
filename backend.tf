f6566e346126d3294edfd1d57e151f43b5f215d8
Kavana1997 <102670832+Kavana1997@users.noreply.github.com>
2025-03-28 18:33:03 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "terraform_state1" {
-  bucket = "my-terraform-state-bucket-unique-name-12345"
+  bucket = "my-terraform-state-bucket-unique-name-123456"
 }
 
 resource "aws_s3_bucket_versioning" "terraform_state" {
@@ -21,7 +21,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" 
 
 # Create DynamoDB table for state locking
 resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-lock-unique-name-12345"
+  name         = "terraform-lock-unique-name-123456"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
